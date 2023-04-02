84d0e7ef485f119b99e33b112686ee3d4c277066
Harshith HM <harshith.hm@cleartax.in>
2023-04-02 22:38:19 +0530

    increase max allowed users to 10

--- games/blackJack/config.ts
+++ games/blackJack/config.ts
@@ -1,6 +1,6 @@
 export default {
   targetScore: 21,
-  maxAllowedPlayers: 5,
+  maxAllowedPlayers: 10,
   maxCardsAllowedPerUser: 5,
   deckApiBaseUrl: `https://www.deckofcardsapi.com`,
   getShuffledNewDeckEndpoint: `/api/deck/new/shuffle/?deck_count=1`,
